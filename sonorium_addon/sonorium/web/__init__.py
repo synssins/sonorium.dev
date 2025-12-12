dc798da2297832d558b59104805eaf3959945342
synssins <synssins@gmail.com>
2025-12-11 18:03:21 -0600

    Update web __init__.py to export app factory

--- sonorium_addon/sonorium/web/__init__.py
+++ sonorium_addon/sonorium/web/__init__.py
@@ -5,7 +5,10 @@ Provides REST API and web UI for managing Sonorium.
 """
 
 from sonorium.web.api_v2 import create_api_router
+from sonorium.web.app import SonoriumApp, create_app
 
 __all__ = [
     "create_api_router",
+    "SonoriumApp",
+    "create_app",
 ]
